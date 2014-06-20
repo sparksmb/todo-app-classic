50f999e1ab3c3cc8d4cd74dc0dd0ee77bda8735d
sparksmb <sparksmb@gmail.com>
2014-06-20 06:27:33 -0500

    updated html text box

--- todo-app/views/todo-list-view.js
+++ todo-app/views/todo-list-view.js
@@ -2,6 +2,14 @@
 app.view.todoListView = {
 	create: function (xhr, iViewData) {
 		'use strict';
+		var viewData = iViewData || {
+				container_id: 'todo-app',
+				template: { url: 'views/todo-list-view.html'},
+				data: {
+					todoList: null
+				}
+			},
+			todoListView = Object.create(app.view.htmlView.create(xhr, viewData));
 		
 		function sendAddItemEvent(text) {
 			$('#todo-app').trigger({
@@ -18,24 +26,38 @@ app.view.todoListView = {
 			});
 		}
 		
-		
+		function initMarkCompleted() {
+		}
 		
 		function init() {
 			initAddItem();
+			initMarkCompleted();
 			$('#addItemTextbox').focus();
 		}
 		
-		var viewData = iViewData || {
-				container_id: 'todo-app',
-				template: { url: 'views/todo-list-view.html'},
-				data: {
-					todoList: null
-				}
-			},
-			todoListView = Object.create(app.view.htmlView.create(xhr, viewData));
+		function preProcessItemsForRendering() {
+			var i,
+				completed = 0,
+				todoItems = viewData.data.todoList,
+				len = todoItems.length;
 			
+			for (i = 0; i < len; i += 1) {
+				if (todoItems[i].isCompleted) {
+					completed += 1;
+				}
+				todoItems[i].index = i;//expose index as a property
+			}
+			
+			todoItems.itemsLeft = len - completed;
+		}
+		
 		todoListView.initEventHandlers = function () {
 			init();
+		};
+		
+		todoListView.render = function () {
+			preProcessItemsForRendering();
+			Object.getPrototypeOf(todoListView).render(); //baseObj.render()
 		};
 		
 		return todoListView;
